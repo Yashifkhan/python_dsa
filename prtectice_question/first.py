08ebc266f79baf8af0a7578387d49237e0d383e6
yashif khan <yashifyashifkhan@gmail.com>
2026-01-15 22:32:36 +0530

    prectice qustion

---
+++ prtectice_question/first.py
@@ -0,0 +1,25 @@
+print("learn python question")
+
+# give an array of integer if any any value apper is at the last twice then return true else false 
+list=[1,2,4,2]
+
+
+# def check_twice(list):
+#     for i in list:
+#         if i == list[-1]:
+#             return True
+#     return False 
+       
+# print(check_twice(list))
+seen=set()
+def check_dup(list):
+    for i in list:
+        if i in seen:
+            return True
+        else :
+            seen.add(i)
+    else :
+        return False
+
+print(check_dup(list))
+print(seen)
