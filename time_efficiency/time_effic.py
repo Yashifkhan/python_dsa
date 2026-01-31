6a9ad40749280449a63bc2586e61c52c614c0a83
yashif khan <yashifyashifkhan@gmail.com>
2026-01-31 23:34:32 +0530

    learn time efficiency

---
+++ time_efficiency/time_effic.py
@@ -0,0 +1,15 @@
+print("learn time efficience")
+
+# print(check_twice(list))
+# seen=set()
+# def check_dup(list):
+#     for i in list:
+#         if i in seen:
+#             return True
+#         else :
+#             seen.add(i)
+#     else :
+#         return False
+
+# print(check_dup(list))
+# print(seen)
